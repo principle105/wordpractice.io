ea9e897a5e7b4a551eab969a6c02db4419153f24
principle105 <principle105@gmail.com>
2024-06-26 17:07:14 -0400

    panother potential fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,18 +6,6 @@ module.exports = {
             node_args: "--env-file .env",
             watch: true,
             env: {
-                PORT: 5173,
-                NODE_ENV: "development",
-                BASE_URL: process.env.BASE_URL,
-                MONGODB_CONNECTION_URI: process.env.MONGODB_CONNECTION_URI,
-                GH_CLIENT_ID: process.env.GH_CLIENT_ID,
-                GH_CLIENT_SECRET: process.env.GH_CLIENT_SECRET,
-                DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-                DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-                GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-                GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-            },
-            env_production: {
                 PORT: 3000,
                 NODE_ENV: "production",
                 BASE_URL: process.env.BASE_URL,
